7bd67682f75046e2343ba52091df051ac1587874
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2016-09-20 09:10:55 +0200

    updated to Android Studio 2.2

--- Kapitel09/AppInstallDemo/app/src/main/java/com/thomaskuenneth/appinstalldemo/ExternalApplicationsAvailableReceiver.java
+++ Kapitel09/AppInstallDemo/app/src/main/java/com/thomaskuenneth/appinstalldemo/ExternalApplicationsAvailableReceiver.java
@@ -7,16 +7,17 @@ import android.util.Log;
 
 public class ExternalApplicationsAvailableReceiver extends BroadcastReceiver {
 	
-	private static final String TAG = ExternalApplicationsAvailableReceiver.class
+	private static final String TAG =
+			ExternalApplicationsAvailableReceiver.class
 			.getSimpleName();
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if (intent != null) {
-			if (Intent.ACTION_EXTERNAL_APPLICATIONS_AVAILABLE.equals(intent
-					.getAction())) {
-				String[] packages = intent
-						.getStringArrayExtra(Intent.EXTRA_CHANGED_PACKAGE_LIST);
+			if (Intent.ACTION_EXTERNAL_APPLICATIONS_AVAILABLE.equals(
+					intent.getAction())) {
+				String[] packages = intent.getStringArrayExtra(
+								Intent.EXTRA_CHANGED_PACKAGE_LIST);
 				for (String pkg : packages) {
 					Log.d(TAG, pkg);
 				}
